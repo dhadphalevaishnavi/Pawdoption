84274466391500babce5e594f377fb93b1d299db
dhadphalevaishnavi <dhadphalevaishnavi@gmail.com>
2022-01-11 20:03:13 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,60 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { QuestionsComponent } from './Components/questions/questions.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { VerifyOTPComponent } from './Components/verify-otp/verify-otp.component';
+import { AddPetComponent } from './Components/addPet/add-pet/add-pet.component';
+import { SearchResultComponent } from './Components/search-result/search-result.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path:"",
+    component:HomeComponent,
+    pathMatch:"full"
+  },
+
+  {
+    path:"Login",
+    component:LoginComponent,
+    pathMatch:"full"
+  },
+
+  {
+    path:"sendOTP",
+    component:RegistrationComponent,
+    pathMatch:"full"
+  },
+
+  {
+    path:"verifyOtp",
+    component:VerifyOTPComponent,
+    
+  },
+
+  {
+    path:"questionForm",
+    component:QuestionsComponent
+  },
+
+  {
+    path:"addPet",
+    component:AddPetComponent
+  },
+
+  {
+    path:'showPets',
+    component:SearchResultComponent
+  }
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes , {onSameUrlNavigation:'reload'} ) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { HomeComponent } from './Components/home/home.component';
+import { VerifyOTPComponent } from './Components/verify-otp/verify-otp.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { QuestionsComponent } from './Components/questions/questions.component';
+import { AddPetComponent } from './Components/addPet/add-pet/add-pet.component';
+import { FooterComponent } from './Components/footer/footer/footer.component';
+import { SearchResultComponent } from './Components/search-result/search-result.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegistrationComponent,
+    HomeComponent,
+    VerifyOTPComponent,
+    QuestionsComponent,
+    AddPetComponent,
+    FooterComponent,
+    SearchResultComponent,
+    SidebarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    NgxPaginationModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
